095c294c2b21a6005ffcb11fb81a7b651420c7d4
User <vartan.andreevich@yandex.ru>
2020-07-27 17:02:30 +0300

    2. Каркас шахматной доски [#311885]

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -17,13 +17,28 @@ public class BishopBlack implements Figure {
 
     @Override
     public Cell[] way(Cell source, Cell dest) {
-        throw new IllegalStateException(
-                String.format("Could not way by diagonal from %s to %s", source, dest)
-        );
+        if (!isDiagonal(position, dest)) {
+            throw new IllegalStateException(
+                    String.format("Could not move by diagonal from %s to %s", source, dest)
+            );
+        }
+        int size = Math.abs(source.getX() - dest.getX());
+        Cell[] steps = new Cell[size];
+        int deltaX = source.getX() - dest.getX() < 0 ? +1 : -1;
+        int deltaY = source.getY() - dest.getY() < 0 ? +1 : -1;
+        steps[0] = Cell.findBy(source.getX() + deltaX, source.getY() + deltaY);
+        for (int index = 1; index < size; index++) {
+                steps[index] = Cell.findBy(steps[index - 1].getX() + deltaX, steps[index - 1].getY() + deltaY);
+        }
+        return steps;
     }
 
     public boolean isDiagonal(Cell source, Cell dest) {
-        return false;
+        if (Math.abs(source.getX() - dest.getX()) != Math.abs(source.getY() - dest.getY())) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     @Override
--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -3,7 +3,10 @@ package ru.job4j.chess;
 import org.junit.Ignore;
 import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
 import ru.job4j.chess.firuges.black.BishopBlack;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class LogicTest {
 
@@ -14,5 +17,50 @@ public class LogicTest {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C1));
         logic.move(Cell.C1, Cell.H6);
+    }
+
+    @Test
+    public void BishopBlackPositionTrue() {
+        BishopBlack figure = new BishopBlack(Cell.A8);
+        assertThat(Cell.A8, is(figure.position()));
+    }
+
+    @Test
+    public void BishopBlackCopyTrue() {
+        BishopBlack figure1 = new BishopBlack(Cell.A3);
+        Figure figure2 = figure1.copy(Cell.A3);
+        assertThat(Cell.A3, is(figure2.position()));
+    }
+
+    @Test
+    public void BishopBlackWayTrue() {
+        BishopBlack figure = new BishopBlack(Cell.C1);
+        Cell[] expectedWay = figure.way(Cell.C1, Cell.G5);
+        Cell[] trueWay = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        assertThat(expectedWay, is(trueWay));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void BishopBlackWayFalse() {
+        BishopBlack figure = new BishopBlack(Cell.C1);
+        Cell[] expectedWay = figure.way(Cell.C1, Cell.C2);
+    }
+
+    @Test(expected = FigureNotFoundException.class)
+        public void FigureNotFound()
+            throws FigureNotFoundException, ImpossibleMoveException, OccupiedCellException {
+        Logic logic = new Logic();
+        logic.move(Cell.C1, Cell.C2);
+    }
+
+    @Test(expected = ImpossibleMoveException.class)
+        public void WhenAFigureStandsInTheWay()
+            throws FigureNotFoundException, ImpossibleMoveException, OccupiedCellException {
+        Logic logic = new Logic();
+        BishopBlack figure1 = new BishopBlack(Cell.A1);
+        logic.add(figure1);
+        BishopBlack figure2 = new BishopBlack(Cell.B2);
+        logic.add(figure2);
+        logic.move(Cell.A1, Cell.C3);
     }
 }
